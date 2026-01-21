857a84e1b1db1891c0385ec274eea1bbfd8dd310
Shivam Kumar <sk744826744@gmail.com>
2026-01-21 13:18:38 +0530

    changes to file structure

---
+++ backend/src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import healthRoutes from "./routes/health.routes.js";
+
+const app = express();
+
+app.use(express.json());
+
+// health check
+app.use("/api/health", healthRoutes);
+
+export default app;
