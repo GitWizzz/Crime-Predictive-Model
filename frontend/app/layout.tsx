5b2191b2358fc76be05a62a70f19eda33c8e3e58
Shivam Kumar <sk744826744@gmail.com>
2026-05-11 09:13:00 +0530

    Final production build fixes (Suspense and Types)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import "./globals.css";
 import TopNav from "@/components/layout/TopNav";
 
@@ -9,7 +10,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
-        <TopNav />
+        <Suspense>
+          <TopNav />
+        </Suspense>
         <div className="min-h-[calc(100vh-56px)]">{children}</div>
       </body>
     </html>
